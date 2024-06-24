5ab09689db45b9538c4f86e72f79e3a76cbb8e6f
MUX_ON_WINDOWS <maxnouts@gmail.com>
2024-06-24 10:21:35 +0200

    Remove

--- php/connection.php
+++ php/connection.php
@@ -8,11 +8,6 @@ ini_set('display_errors', 1);
 header('Cache-Control: no-cache, must-revalidate');
 header('Content-type: application/json');
 
-$host = "rdbms.strato.de";
-$username = "dbu1032517";
-$password = "Password1234!";
-$database = "dbs12945069";
-
 $conn = new mysqli($host, $username, $password, $database);
 
 if ($conn->connect_error) {
